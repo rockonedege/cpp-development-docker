97a0de9b566b47dc7148260f1d8e2c8c55a2eff4
ttan <rockonedge@live.com>
2019-11-18 15:44:38 +0800

    refactored script

--- example/run-container.py
+++ example/run-container.py
@@ -4,40 +4,45 @@ import re
 
 
 this_folder = Path(__file__).absolute().parent
-workspace = this_folder.parent
 
 
-def run(image):
-    compiler = re.sub(r'(^.+/)|(:+.$)', '', image)
-    cmd = f'''docker run -it --name {compiler} \
+def make_commands(images, workspace):
+    def compiler(x): return re.sub(r'(^.+/)|(:+.$)', '', x)
+    return [f'''docker run -it --name {compiler(image)} \
 -v {workspace}:/workspace \
 -v {this_folder}:/scripts \
 {image}:latest  \
 /bin/bash "/scripts/init.sh"
-'''
+''' for image in images]
+
+
+def run_one(cmd):
     print(cmd)
     subprocess.run(cmd, shell=True)
 
 
-def main(use_local):
+def main(use_local, workspace):
     import concurrent.futures
 
-    # locally built images
-    local_images = ['local/gcc9', 'local/clang9']  
+        # locally built images
+    local_images = ['local/gcc9', 'local/clang9']
     # pre-built images on docker hub
     remote_images = ['tomgee/cpp-dev-gcc', 'tomgee/cpp-dev-clang']
-
     images = local_images if use_local else remote_images
 
+    commands = make_commands(images, workspace)
+
     with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
-        executor.map(run, images)
+        executor.map(run_one, commands)
 
     print(
-        f'''done with {images}: 
+        f'''done with {images}:
     {workspace} ==> /workspace
     {this_folder} ==> /scripts
 ''')
 
 
 if __name__ == "__main__":
-    main(use_local=True)
+
+    default_workspace = this_folder.parent
+    main(use_local=False, workspace='default_workspace')
